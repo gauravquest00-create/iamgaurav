c8440b1def6c9462d8f107f5e3b5c2a55eb80c7a
BlueBuck <gauravquest00@gmail.com>
2026-08-22 10:10:57 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -107,7 +107,7 @@ export const Navbar = () => {
         {/* LOGO (ALWAYS VISIBLE) */}
         <NavLink to="/" className="navbar-brand" aria-label="Gaurav Portfolio Home">
           <span className="brand-name">GAURAV</span>
-          <span className="brand-tag">DEV</span>
+          <span className="brand-tag">SYSTEMS</span>
         </NavLink>
 
         {/* DESKTOP NAVIGATION (HIDDEN ON MOBILE) */}
